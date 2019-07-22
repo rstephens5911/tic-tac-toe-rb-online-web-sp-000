0703cd4a8e8693536b6a1ec5aedc53a671c876f0
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-22 17:12:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    true
-  end
+  index.between?(0,8) && !position_taken?(board, index)
 end
