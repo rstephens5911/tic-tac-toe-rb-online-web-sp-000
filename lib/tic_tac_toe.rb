393bb99db6ec64888cd5ab3c6b170a4ff685ce0d
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-22 17:04:36 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,26 @@
+# Tic Tac Toe
+#WIN_COMBINATIOSN constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottome row
+  #------
+  [0,3,6], # First column
+  [1,4,7], # Second column
+  [2,5,8], # Third column
+  #------
+  [0,4,8], # Left diagonal
+  [2,4,6]  # Right diagonal
+]
+
+# initialize board array
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# display_board is the method to build the board for output
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " -----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " -----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
